76f995683e3a45d9e339b36303db571445099a1e
Dally Wally <dallin.watac.major@gmail.com>
2018-07-22 16:46:58 -0600

    I have it mostly working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,28 @@ module.exports = function(app) {
     })
 
     app.post("/api/friends", function(req, res) {
+        var user = req.body;
+        var friendMatch;
+        var friendMatchDiff = 100;
+
+        friends.forEach(function(friend) {
+            var difference = 0
+
+            for (var i = 0; i > friend.scores.length; i++) {
+                 if (friend.scores[i] >= user.scores[i]) {
+                    difference = difference + friend.scores[i] - user.scores[i];
+                 } else {
+                    difference = difference + user.scores[i] - friend.scores[i];
+                 }
+            }
+
+            if ( difference < friendMatchDiff) {
+                friendMatch = friend;
+                friendMatchDiff = difference;
+            }
+        })
+
         friends.push(req.body);
+        res.send(friendMatch);
     })
 }
