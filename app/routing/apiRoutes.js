91155f008a66c95df8ec5b09f24ed1c5735ae3a7
Dally Wally <dallin.watac.major@gmail.com>
2018-07-24 19:03:14 -0600

    Fixed bugs. It should work now

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,42 @@
 var friends = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends)
     })
 
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
         var user = req.body;
         var friendMatch;
         var friendMatchDiff = 100;
 
-        friends.forEach(function(friend) {
-            var difference = 0
+        friends.forEach(function (friend) {
+            var difference = 0;
+            console.log("Friend Match Diff" + friendMatchDiff);
 
-            for (var i = 0; i > friend.scores.length; i++) {
-                 if (friend.scores[i] >= user.scores[i]) {
-                    difference = difference + friend.scores[i] - user.scores[i];
-                 } else {
-                    difference = difference + user.scores[i] - friend.scores[i];
-                 }
-            }
+            if (friend.name === user.name) {
+                return
+            } else {
 
-            if ( difference < friendMatchDiff) {
-                friendMatch = friend;
-                friendMatchDiff = difference;
+                for (var i = 0; i < friend.scores.length; i++) {
+                    console.log(difference);
+                    if (friend.scores[i] >= user.scores[i]) {
+                        difference = difference + friend.scores[i] - parseInt(user.scores[i]);
+                    } else {
+                        difference = difference + parseInt(user.scores[i]) - friend.scores[i];
+                    }
+                }
+
+                if (difference < friendMatchDiff) {
+                    friendMatch = friend;
+                    friendMatchDiff = difference;
+                }
+                console.log(friendMatch);
             }
         })
-
-        friends.push(req.body);
+        console.log(friendMatch);
+        friends.push(user);
         res.send(friendMatch);
     })
 }
